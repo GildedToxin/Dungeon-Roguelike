2b611ef866e34e8d48dcb891d040f5dd123f272d
PranavAlla <Pranavalla@gmail.com>
2026-06-22 18:23:20 -0400

    Saving work on projectiles

--- dungeon-roguelike/Scripts/projectile_script.gd
+++ dungeon-roguelike/Scripts/projectile_script.gd
@@ -1,11 +1,16 @@
-extends Node
+extends RigidBody3D
 
+var speed: float = 1
+var duration: float = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func shoot(direction: Vector3) -> void:
+	apply_impulse(direction * speed)
