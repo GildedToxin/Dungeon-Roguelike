f6a9c7af5f4ee52ed04f603e2c83e3de8ba231a8
Toxin4426 <Toxin4426@gmail.com>
2026-06-22 18:08:28 -0400

    fix multiplayer

--- dungeon-roguelike/Scenes/CameraRig.gd
+++ dungeon-roguelike/Scenes/CameraRig.gd
@@ -3,6 +3,18 @@ extends Node3D
 @export var target: CharacterBody3D
 @export var follow_speed: float = 5.0
 @export var offset: Vector3 = Vector3(0, 0, 0)
+@onready var camera: Camera3D = $Camera3D
+
+
+
+func _enter_tree() -> void:
+	set_multiplayer_authority(int(name))
+
+func _ready() -> void:
+	if not is_multiplayer_authority():
+		camera.current = false
+		set_process(false)
+		set_physics_process(false)
 
 func _physics_process(delta):
 	if target == null:
