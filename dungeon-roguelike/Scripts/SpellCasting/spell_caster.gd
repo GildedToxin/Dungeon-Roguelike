e4a04ce939681fceffda2cf53604903a3864e99a
PranavAlla <Pranavalla@gmail.com>
2026-06-24 11:26:26 -0400

    fixed projectile spawning

--- dungeon-roguelike/Scripts/SpellCasting/spell_caster.gd
+++ dungeon-roguelike/Scripts/SpellCasting/spell_caster.gd
@@ -1,10 +1,12 @@
 # Goes on the player, creates the projectiles for spells
-extends Node
+extends Node3D
 
 @export var projectile_scene: PackedScene
 
 func cast() -> void:
 	var projectile = projectile_scene.instantiate()
+	get_tree().current_scene.add_child(projectile)
+	projectile.global_position = global_position
 	projectile.shoot(Vector3(0, 1, 1))
 
 func _input(event):
